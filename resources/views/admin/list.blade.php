bc4403b04415c5ee753a24ac73480419fc5f70a3
Konrad Riedel <riedel@ibr-oss.de>
2016-09-14 22:58:20 +0200

    Spaltenreihenfolge

--- resources/views/admin/list.blade.php
+++ resources/views/admin/list.blade.php
@@ -6,33 +6,32 @@
 <div class="container">
     <br /><br />
      <div class="col-md-12">
-       <a href="{{ route('admin/add') }}"<button type="button" class="btn btn-sm btn-success">Add New Faq Entry</button></a>
+       <a href="{{ route('admin/add') }}"<button type="button" class="btn btn-sm btn-success">Neuer Eintrag</button></a>
    <br /><br />
         <table class="table table-bordered">
             <thead>
                 <tr>
-                    <th>#</th>
+                    <th>Kategorie</th>
                     <th>Schlagwort</th>
+                    <th>#</th>
                     <th>Frage</th>
                     <th>Antwort</th>
-                    <th>Kategorie</th>
                     <th>Referenz</th>
-                    <th>Action</th>
+                    <th>Aktion</th>
                 </tr>
             </thead>
  @if (count($faqs) > 0)
             <tbody>
                 @foreach ($faqs as $faq)
                 <tr>
-                    <td>{{ $faq->id }}</td>
+                    <td>{{ $faq->kategorie->kz }}</td>
                     <td>{{ $faq->schlagwort }}</td>
+                    <td>{{ $faq->id }}</td>
                     <td>{{ $faq->frage }}</td>
                     <td>{{ $faq->antwort }}</td>
-                    <td>{{ $faq->kategorie->kategorie }}</td>
                     <td>{{ $faq->referenz }}</td>
                     <td>
-                    <a href="{{route('admin/edit',['id'=>$faq->id]) }}" class="editrow">Edit</a>&nbsp;&nbsp;|
-                    <a href="{{route('admin/delete',['id'=>$faq->id]) }}" class="deleterow">Delete</a>
+                    <a href="{{route('admin/edit',['id'=>$faq->id]) }}" class="editrow">Bearbeiten</a>
                     </td>
                 </tr>
                 @endforeach
